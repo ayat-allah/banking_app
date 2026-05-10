31c9ab8891e145e7bdcaf1f40ebe826f57e5cdae
ayat-allah <166665397+ayat-allah@users.noreply.github.com>
2026-05-10 07:00:45 +0300

    Add files via upload

---
+++ frontend-service/src/main/java/com/banking/front/FrontApplication.java
@@ -0,0 +1,13 @@
+package com.banking.front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontApplication.class, args);
+	}
+
+}
